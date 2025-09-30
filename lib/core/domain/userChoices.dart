1af9117dbc049398cb59f5eecbae291a101be8a0
AlyanaKtk <varkatkantas413@gmail.com>
2025-09-30 05:18:02 +0300

    changed json, added bool functions prototypes

---
+++ lib/core/domain/userChoices.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart';
+
+bool wordsChoice(List<String> keywords, List<String> userWords)
+{
+  return assert(listEquals(keywords, userWords));
+
+}
+
+bool isResultCorrect(String userResult, String expectedResult) //первое ввод юзера, второе значение из json
+{
+  return userResult==expectedResult;
+
+}
